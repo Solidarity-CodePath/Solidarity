15dc3a5dde61fba4c9c5d776994a4ca01b73d724
shreyashekhar <shreya.s.shekhar@gmail.com>
2020-07-17 16:11:08 -0700

    added profile image uploading

--- app/src/main/java/com/example/solidarity/ProfileFragment.java
+++ app/src/main/java/com/example/solidarity/ProfileFragment.java
@@ -1,26 +1,53 @@
 package com.example.solidarity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.example.solidarity.fragments.EventsFragment;
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 
 
 public class ProfileFragment extends EventsFragment {
 
     public static final String TAG = "ProfileFragment";
+    public static final int GET_FROM_GALLERY = 3;
+    CircleImageView profImage;
+
+    public String photoFileName = "photo.jpg";
+    File photoFile;
+    ParseUser currentUser;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +55,54 @@ public class ProfileFragment extends EventsFragment {
         setHasOptionsMenu(true);
         queryEvents();
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FrameLayout flayout = view.findViewById(R.id.fLayout);
+        profImage = new CircleImageView(getContext());
+        profImage.setImageResource(R.drawable.camera);
+        flayout.addView(profImage, 300, 300);
+
+        profImage.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.
+                        MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==GET_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), selectedImage);
+                profImage.setImageBitmap(bitmap);
+                photoFile = new File(selectedImage.getPath());
+                currentUser = ParseUser.getCurrentUser();
+                
+                //saveUser(currentUser, photoFile);
+
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                Log.e(TAG, "File not found!");
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                Log.e(TAG, "IOException!");
+            }
+        }
+    }
+
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
@@ -71,5 +146,32 @@ public class ProfileFragment extends EventsFragment {
 
             }
         });
+    }
+
+    private void saveUser(ParseUser currentUser, File photoFile) {
+        ParseFile file = new ParseFile(photoFile);
+        currentUser.put("profileImage", file);
+
+        file.saveInBackground(new SaveCallback() {
+            public void done(ParseException e) {
+
+                if(e != null) {
+                    Log.e(TAG, "Error while saving parsefile", e);
+                    Toast.makeText(getContext(), "Error while saving parsefile!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        currentUser.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Error while saving", e);
+                    Toast.makeText(getContext(), "Error while saving!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
