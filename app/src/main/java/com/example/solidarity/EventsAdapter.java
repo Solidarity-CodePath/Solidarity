c0fd93de18805a8a49b7c3b1d4077ce928b03d2f
shreyashekhar <shreya.s.shekhar@gmail.com>
2020-08-03 12:47:01 -0700

    added google maps directions funcitonality for events

--- app/src/main/java/com/example/solidarity/EventsAdapter.java
+++ app/src/main/java/com/example/solidarity/EventsAdapter.java
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.solidarity.fragments.EventsFragment;
 import com.parse.ParseFile;
 
 import org.parceler.Parcels;
@@ -120,6 +121,7 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.ViewHolder
                     Intent intent = new Intent(context, EventDetailsActivity.class);
                     intent.putExtra(Event.class.getSimpleName(), Parcels.wrap(event));
                     intent.putExtra("position", getAdapterPosition());
+
                     fragment.startActivityForResult(intent, REQUEST_CODE);
                 }
             });
